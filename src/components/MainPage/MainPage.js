aad678df7846b2908c09388c2ff3447f26fc2514
Simon Ljungström <simonlj8@gmail.com>
2023-01-28 21:39:22 +0100

    Add a signUp page and start some refactoring

---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import LogIn from "../Login/Login";
+import SignUp from "../SignUp/SignUp";
+import '../../App.css'
+
+
+
+function mainPage() {
+
+    //const [startPage, setStartPage] = useState('login')
+
+   /* const handleSubmitSignUp = (e) => {
+      e.preventDefault();
+      setStartPage=('signup');
+    }*/
+
+    
+    return (
+        <>
+        
+        
+        
+
+        </>
+    );
+};
+
+export default mainPage;
